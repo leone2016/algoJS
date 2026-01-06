fe9c523e5900499dc52c1186f3cbfee352c96f4d
Leonardo Medina <leoz.31@hotmail.com>
2026-01-06 14:35:00 -0600

    feat(tree): preOrder

--- Tree/BinaryTree.ts
+++ Tree/BinaryTree.ts
@@ -56,4 +56,18 @@ export class BinaryTree {
         return out;
     }
 
+    preOrder(): number[]{
+        if(!this.root) return [];
+        const out: number[] = [];
+        const q: TreeNode[] = [this.root];
+        while(q.length > 0){
+            const pointer = q.shift();
+            if(!pointer) break;
+            out.push(pointer.data);
+            if(pointer.lchild) q.push(pointer.lchild);
+            if(pointer.rchild) q.push(pointer.rchild);
+        }
+        return out;
+    }
+
 }
